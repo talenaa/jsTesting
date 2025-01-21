d126a92d4788314f2c77385df1c72754fe58ece4
talenaa <talenabarbosa7@gmail.com>
2025-01-21 15:38:01 +0100

    starting with basic declaration

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ console.log(`hello`);
 console.log(`I like videogames`);
 console.log('Crystal');
 
-window.alert(`java is a nightmare`);
-window.alert(`programming is cool`);
+/* window.alert(`java is a nightmare`);
+window.alert(`programming is cool`); */
 
 document.getElementById("intro").textContent = `hello`;
 document.getElementById("paragraph").textContent = `Im a green day fan`;
+
+/* Variable = A container that stores a value, this value can change later on, depending on the conditions or on information passed to the program 
+  Behaves as if it were the value it contains
+*/
+
+// The two principal steps to create a variable are:
+// 1. declaration  let x;
+// 2. assignment   x = 100;
+
+// Every variable name needs to be unique.
+// If we declare two variables with the same name, the console output will be an error.
+
+let x;
+x = 100;
+
+console.log(x);
+
+// to do both declaration and assignment at the same time, it goes something like this
+
+let y = 443;
+
+console.log(y);
